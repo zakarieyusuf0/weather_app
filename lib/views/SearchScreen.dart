7ff76befb815ac176871a3505a9be9abb9f09276
ZakariyeYusuf09 <zakariyeyuusuf617@gmail.com>
2024-10-08 21:23:04 +0300

    added some functionality also api key

--- lib/views/SearchScreen.dart
+++ lib/views/SearchScreen.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 import 'package:flutter/material.dart';
+import 'package:weather/weather.dart';
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
@@ -9,45 +10,76 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
+  Weather? weather;
+  final openWeather = WeatherFactory('e772dea156dc6287189424005ac4cd55');
+  String cityname = '';
+  getweather() async {
+    try {
+      weather = await openWeather.currentWeatherByCityName(cityname);
+      setState(() {});
+    } catch (e) {
+      print('Enter Valid City $e ');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        backgroundColor: Colors.lightBlue,
+        title: Text('Search the city you like'),
+      ),
       body: Column(
         children: [
           Container(
             margin: EdgeInsets.symmetric(
               horizontal: 15,
+              vertical: 10,
             ),
             child: TextField(
+              onChanged: (value) {
+                setState(() {
+                  cityname = value;
+                });
+              },
               decoration: InputDecoration(
                 hintText: 'Please Enter The City',
-                suffix: IconButton(
-                  onPressed: () {},
-                  icon: Icon(
-                    Icons.cloud,
-                  ),
-                ),
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(
                     10,
+                  ),
+                ),
+                suffix: IconButton(
+                  onPressed: () {
+                    getweather();
+                  },
+                  icon: Icon(
+                    Icons.search,
                   ),
                 ),
               ),
             ),
           ),
           Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'City',
-                ),
-                Text(
-                  'Faah-Faahin',
-                ),
-              ],
-            ),
+            child: weather != null
+                ? Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        '${weather!.temperature!.celsius!.round()} °C ',
+                        style: TextStyle(
+                          fontSize: 35,
+                        ),
+                      ),
+                      Text(
+                        '${weather!.weatherDescription}',
+                        style: TextStyle(
+                          fontSize: 35,
+                        ),
+                      ),
+                    ],
+                  )
+                : SizedBox(),
           ),
         ],
       ),
